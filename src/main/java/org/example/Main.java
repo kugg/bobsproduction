eaa2ab69f2d381b4c481282d44b9e4bd30b70371
Christoffer Jerkeby <kugghjul@gmail.com>
2024-09-10 22:03:03 +0200

    Spelling in prod, fixing tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,6 @@ import java.nio.file.Path;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    Thing t = new Thing();
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
---
+++ src/test/java/org/example/ThingTests.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.Test;
+
+public class ThingTests {
+    @Test
+    public void TestTheThing() {
+        Thing T = new Thing();
+    }
+}
