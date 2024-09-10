aa27f67fda23637768aa6fe4aaa9b027ea3ebd3f
Christoffer Jerkeby <kugghjul@gmail.com>
2024-09-10 21:56:36 +0200

    Spelling in prod

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
-import org.apache.commons.io.file.PathUtils;
+import org.apache.commons.io.ByteOrderMark;
+
 
 import java.nio.file.Path;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    Thing t = new Thing();
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Path path = PathUtils.current();
-        System.out.println(path);
-        System.out.println("Fancy produciton2");
+        System.out.println("Fancy production");
     }
 }
---
+++ src/main/java/org/example/Thing.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.apache.commons.io.ByteOrderMark;
+
+public class Thing {
+        public Thing () {
+        new ByteOrderMark("utf-8");
+    }
+}
