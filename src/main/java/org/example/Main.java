9af0a7aefc80a1135e6e8392d18fc38d17033c9b
Christoffer Jerkeby <kugghjul@gmail.com>
2024-09-10 20:46:35 +0200

    Added fancy production code.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.apache.commons.io.file.PathUtils;
+
+import java.nio.file.Path;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Path path = PathUtils.current();
+        System.out.println(path);
+    }
+}
